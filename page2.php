7d641cb75c47a766bf6ff323f400cb8e6c35cacb
dragonic4 <max]_william02@hotmail.com>
2023-01-18 14:39:12 -0300

    novo

--- page2.php
+++ page2.php
@@ -1,13 +1,4 @@
 
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    $nome = $_POST['nome'];
-    $pass = $_POST['senha'];
-    if(empty($nome)){
-        echo "vazio";
-    }else{
-        echo $nome . $pass; 
-    }
-}
+echo "hello world!";
 ?>
